ea4da7ac93dbc5a4df60479c60a6cdf88f0e753f
Vladislav Ulyanov <flynnrcore@yandex.ru>
2024-02-01 11:47:33 +0300

    add video content

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -2,9 +2,17 @@ const VideoSection = () => {
   return (
     <div className="video-section">
       <iframe
+      className="video"
+      src="https://www.youtube.com/embed/ZLaS0psJOCw?si=EktXJDqNz18mg1lL"
+      title="YouTube"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+      />
+      <iframe
         className="video"
         src="https://www.youtube.com/embed/gi-LhzHWF-Y?si=W5H1jxLyjG406e1Z&amp;controls=0"
-        title="YouTube video player"
+        title="YouTube"
         frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowfullscreen
       />
