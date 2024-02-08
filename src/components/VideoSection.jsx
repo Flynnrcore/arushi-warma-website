b453a8c2e48fb7913993e55684768ff352ff1a89
Vladislav Ulyanov <flynnrcore@yandex.ru>
2024-02-08 12:54:23 +0300

    change second video url

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -10,10 +10,11 @@ const VideoSection = () => {
       allowfullscreen
       />
       <iframe
-        className="video"
-        src="https://www.youtube.com/embed/gi-LhzHWF-Y?si=W5H1jxLyjG406e1Z&amp;controls=0"
-        title="YouTube"
-        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        className="video" 
+        src="https://www.youtube.com/embed/z6re6Qmoafw?si=YZLnLATYQAZRDofa"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowfullscreen
       />
     </div>
