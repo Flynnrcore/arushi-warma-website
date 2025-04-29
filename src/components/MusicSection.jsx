6ead8353a4c4a807071a64d34f5538e7083e79cb
Vladislav Ulyanov <flynnrcore@yandex.ru>
2025-04-29 16:43:13 +0300

    Рефакторинг компонента

--- src/components/MusicSection.jsx
+++ src/components/MusicSection.jsx
@@ -1,50 +1,47 @@
+function MusicItem({ isNew = false, href, children }) {
+  return (
+    <article className="single">
+      <div className="single-wrapper">
+        {isNew && <img className="fire-chip" src="/fire.gif" alt="New release" style={{ zIndex: 2 }} />}
+        {children}
+      </div>
+      <a className="release-btn" href={href} rel="noopener noreferrer">
+        <img
+          className="btn-img"
+          src="/headphones.svg"
+          alt="music release link"
+        />
+      </a>
+    </article>
+  );
+}
+
 function MusicSection() {
   return (
     <section className="music-section">
       <h2 className="header-text">музыка / music</h2>
       <div className="releases">
-        <article className="single">
+        <MusicItem href="https://sfm.click/48Ni">
           <img
             src="/music-releases/2_track.webp"
             alt="single more than justice demands"
             className="release-image"
           />
-          <a className="release-btn" href="https://sfm.click/48Ni">
-            <img
-              className="btn-img"
-              src="/headphones.svg"
-              alt="music release link"
-            />
-          </a>
-        </article>
-        <article className="single">
+        </MusicItem>
+        <MusicItem isNew href="/">
           <img
             src="/music-releases/3_track.webp"
             alt="fox hunt single"
             className="release-image"
           />
-          <a className="release-btn" href="/">
-            <img
-              className="btn-img"
-              src="/headphones.svg"
-              alt="music release link"
-            />
-          </a>
-        </article>
-        <article className="single">
+        </MusicItem>
+        <MusicItem href="https://sfm.click/Ronj">
           <img
             src="/music-releases/1_track.webp"
             alt="single to become a human being"
             className="release-image"
           />
-          <a className="release-btn" href="https://sfm.click/Ronj">
-            <img
-              className="btn-img"
-              src="/headphones.svg"
-              alt="music release link"
-            />
-          </a>
-        </article>
+        </MusicItem>
       </div>
     </section>
   );
